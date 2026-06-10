6c53749abc7610efb75a74b4d02c79fcd2a1bb9c
magasinc <magasinc@espol.edu.ec>
2026-06-10 20:00:28 +0000

    Versionamiento carga productos

--- js/file01.js
+++ js/file01.js
@@ -1,4 +1,49 @@
+import { fetchProducts } from "./functions";
+
 "use strict";
+
+const renderPoducts = () => {
+    fetchProducts('https://data-dawm.github.io/datum/reseller/products.json')
+        .then(result => {
+            if (result.success) {
+                let container = document.getElementById("products-container")
+                container.innerHTML = '';
+
+                let products = result.body.slice(0, 6);
+
+                products.forEach(product => {
+                    let productHTML = `
+   <div class="space-y-4 bg-white dark:bg-gray-800 p-4 rounded-2xl shadow">
+       <img
+           class="w-full h-40 bg-gray-300 dark:bg-gray-700 rounded-lg object-cover transition-transform duration-300 hover:scale-[1.03]"
+           src="[PRODUCT.IMGURL]" alt="[PRODUCT.TITLE]">
+       <h3
+           class="h-6 text-xl font-semibold tracking-tight text-gray-900 dark:text-white hover:text-black-600 dark:hover:text-white-400">
+           $[PRODUCT.PRICE]
+       </h3>
+
+       <div class="h-5 rounded w-full">[PRODUCT.TITLE]</div>
+           <div class="space-y-2">
+               <a href="[PRODUCT.PRODUCTURL]" target="_blank" rel="noopener noreferrer"
+               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full inline-block">
+                   Ver en Amazon
+               </a>
+               <div class="hidden"><span class="1">[PRODUCT.CATEGORY_ID]</span></div>
+           </div>
+       </div>
+   </div>`;
+                    productHTML = productHTML.replaceAll("[PRODUCT.TITLE]", product.title);
+
+                    productHTML = productHTML.replaceAll("[PRODUCT.CATEGORY_ID]", product.category_id);
+                    productHTML = productHTML.replaceAll("[PRODUCT.IMGURL]", product.imgUrl);
+                    container.innerHTML += productHTML
+
+                })
+            }
+        })
+
+}
+
 
 /**
  * Muestra el elemento de notificación interactiva (Toast) en la interfaz de usuario
@@ -31,4 +76,5 @@ const showVideo = () => {
 (() => {
     showToast();
     showVideo();
+    renderPoducts();
 })();
